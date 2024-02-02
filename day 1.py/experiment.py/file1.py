d3d1252e70a6b5e6863f5fa2a6f522d9dcae5764
iliko <iliarajebashvili0@gmail.com>
2024-02-02 19:36:00 +0300

    first commit

---
+++ day 1.py/experiment.py/file1.py
@@ -0,0 +1,17 @@
+name="ilia"
+print(name)
+age=13
+print(age)
+surname="rajebashvili "
+print(surname)
+print(name + surname)
+heigh=1,65
+print(heigh)
+code="jetia123"
+print(code)
+lie="bad"
+print(lie)
+location="gavazi"
+print(location)
+print(name+surname+location+code)
+print(age+heigh)
